8e8355fa570142b6b5a51af4e04741e4b4354f65
Nilton Vasques <niltonvasques@users.noreply.github.com>
2017-01-29 18:07:56 -0300

    Sign in (#1)
    * Create sign in tests

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe GraphqlController, type: :controller do
+  context 'create' do
+    let(:credentials) do
+      { email: @user.email, password: @password }
+    end
+
+    let(:sign_in) do
+      mutation = <<EOF
+        mutation signIn($input: SignInInput!) {
+          signIn(input: $input) {
+            token
+          }
+        }
+EOF
+      variables = { input: credentials }.to_json
+      post :query, params: { query: mutation, variables: variables }, format: :json
+    end
+
+    describe 'when try sign in' do
+      before do
+        @user = create(:user)
+        @password = 'foobar'
+      end
+      describe 'with good credentials' do
+        before do
+          @user.sign_in(@password)
+
+          @expected = {
+            data: {
+              signIn: {
+                token: @user.token
+              }
+            }
+          }
+          sign_in
+        end
+        it { expect(response.body).to be_eql @expected.to_json }
+      end
+
+      describe 'with bad credentials' do
+        before do
+          @expected = {
+            error: 'Bad credentials'
+          }
+          @password = 'wrong'
+          sign_in
+        end
+        it { expect(response.body).to be_eql @expected.to_json }
+      end
+    end
+  end
+end
