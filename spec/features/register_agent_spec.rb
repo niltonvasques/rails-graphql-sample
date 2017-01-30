89c96a59469139765fb5e297f7e5405394b96e39
Nilton Vasques <nilton.vasques@openmailbox.org>
2017-01-30 00:20:46 -0300

    * Refactor register admin, agent

--- spec/features/register_agent_spec.rb
+++ spec/features/register_agent_spec.rb
@@ -1,48 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe GraphqlController, type: :controller do
-  context 'create' do
-    let(:user) do
-      { name: 'test', email: 'test@test.com', password: '123456', password_confirmation: '123456' }
-    end
+  before { sign_in_as_admin }
 
-    let(:register_agent) do
-      mutation = <<EOF
-        mutation registerAgent($input: RegisterAgentInput!) {
-          registerAgent(input: $input) {
-            agent { id, name, email, customer, agent, admin }
-          }
-        }
+  mutation = <<EOF
+    mutation registerAgent($input: RegisterAgentInput!) {
+      registerAgent(input: $input) {
+        agent { id, name, email, customer, agent, admin }
+      }
+    }
 EOF
-      variables = { input: user }.to_json
-      post :query, params: { query: mutation, variables: variables }, format: :json
-    end
+  input = { name: 'test', email: 'test@test.com', password: '123456', 
+            password_confirmation: '123456' }
 
-    describe 'when try register a agent' do
-      before { sign_in_as_admin }
+  expected = { 
+    registerAgent: {
+      agent: {
+        id: '1',
+        name: input[:name],
+        email: input[:email],
+        customer: false,
+        agent: true,
+        admin: false
+      }
+    }
+  }
 
-      it { expect { register_agent }.to change(User, :count).by(1) }
-
-      describe 'and evaluate the result' do
-        before do
-          @expected = {
-            data: {
-              registerAgent: {
-                agent: {
-                  id: '1',
-                  name: user[:name],
-                  email: user[:email],
-                  customer: false,
-                  agent: true,
-                  admin: false
-                }
-              }
-            }
-          }
-          register_agent
-        end
-        it { expect(response.body).to be_eql @expected.to_json }
-      end
-    end
-  end
+  it_behaves_like 'a creatable mutation', User, mutation, input, expected
 end
