f6d2a2bd696d02ba9116685cbd1c11579ed42c6f
Nilton Vasques <nilton.vasques@openmailbox.org>
2017-01-30 00:17:26 -0300

    Refactor mutation specs
    * Extract mutation specs logic to a shared example

--- spec/features/create_request_spec.rb
+++ spec/features/create_request_spec.rb
@@ -1,46 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe GraphqlController, type: :controller do
-  context 'create' do
-    let(:request) do
-      { title: 'title', content: 'content' }
-    end
+  before { @user = sign_in_as_customer }
 
-    let(:create_request) do
-      mutation = <<EOF
-        mutation createRequest($input: CreateRequestInput!) {
-          createRequest(input: $input) {
-            request { id, title, content, user { id } }
-          }
-        }
+  mutation = <<EOF
+    mutation createRequest($input: CreateRequestInput!) {
+      createRequest(input: $input) {
+        request { id, title, content, user { id } }
+      }
+    }
 EOF
-      variables = { input: request }.to_json
-      post :query, params: { query: mutation, variables: variables }, format: :json
-    end
+  input = { title: 'title', content: 'content' }
+  expected = { 
+    createRequest: {
+      request: {
+        id: '1',
+        title: input[:title],
+        content: input[:content],
+        user: { id: "1002" }
+      }
+    }
+  }
 
-    describe 'when try create a request' do
-      before { @user = sign_in_as_customer }
-
-      it { expect { create_request }.to change(Request, :count).by(1) }
-
-      describe 'and evaluate the result' do
-        before do
-          @expected = {
-            data: {
-              createRequest: {
-                request: {
-                  id: '1',
-                  title: request[:title],
-                  content: request[:content],
-                  user: { id: @user.id.to_s }
-                }
-              }
-            }
-          }
-          create_request
-        end
-        it { expect(response.body).to be_eql @expected.to_json }
-      end
-    end
-  end
+  it_behaves_like 'a creatable mutation', Request, mutation, input, expected
 end
--- spec/features/register_user_spec.rb
+++ spec/features/register_user_spec.rb
@@ -1,43 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe GraphqlController, type: :controller do
-  context 'create' do
-    let(:user) do
-      { name: 'test', email: 'test@test.com', password: '123456', password_confirmation: '123456' }
-    end
-
-    let(:register_user) do
-      mutation = <<EOF
-        mutation registerUser($input: RegisterUserInput!) {
-          registerUser(input: $input) {
-            user { id, name, email }
-          }
-        }
+  mutation = <<EOF
+    mutation registerUser($input: RegisterUserInput!) {
+      registerUser(input: $input) {
+        user { id, name, email }
+      }
+    }
 EOF
-      variables = { input: user }.to_json
-      post :query, params: { query: mutation, variables: variables }, format: :json
-    end
+  input = { name: 'test', email: 'test@test.com', password: '123456', 
+            password_confirmation: '123456' }
+  expected = { 
+    registerUser: {
+      user: {
+        id: '1',
+        name: input[:name],
+        email: input[:email]
+      }
+    }
+  }
 
-    describe 'when try register a user' do
-      it { expect { register_user }.to change(User, :count).by(1) }
-
-      describe 'and evaluate the result' do
-        before do
-          @expected = {
-            data: {
-              registerUser: {
-                user: {
-                  id: '1',
-                  name: user[:name],
-                  email: user[:email]
-                }
-              }
-            }
-          }
-          register_user
-        end
-        it { expect(response.body).to be_eql @expected.to_json }
-      end
-    end
-  end
+  it_behaves_like 'a creatable mutation', User, mutation, input, expected
 end
